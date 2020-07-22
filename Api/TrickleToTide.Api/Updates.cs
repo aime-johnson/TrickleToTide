82d175dc7143990067a1e3de0652bf8de1d2238c
Pj <pj@theswamp.co.uk>
2020-07-22 21:30:00 +0100

    ...

--- Api/TrickleToTide.Api/Updates.cs
+++ Api/TrickleToTide.Api/Updates.cs
@@ -10,11 +10,11 @@ using Newtonsoft.Json;
 
 namespace TrickleToTide.Api
 {
-    public static class Function1
+    public static class Updates
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("update")]
+        public static async Task<IActionResult> Post(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
