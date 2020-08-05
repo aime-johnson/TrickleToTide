b3211ba2c0f841f37ff2281a832aded12ff9622c
Pete Whitby <pete.whitby@octavian.co.uk>
2020-08-05 12:22:27 +0100

    Map now zooms/pans to fit all pins

--- Mobile/TrickleToTide.Mobile/ViewModels/MainViewModel.cs
+++ Mobile/TrickleToTide.Mobile/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Shiny;
 using Shiny.Locations;
 using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using TrickleToTide.Common;
 using TrickleToTide.Mobile.Interfaces;
@@ -12,6 +13,7 @@ namespace TrickleToTide.Mobile.ViewModels
     class MainViewModel : BaseViewModel
     {
         private readonly ILocationUpdates _updates;
+        private readonly ObservableCollection<PositionViewModel> _positions = new ObservableCollection<PositionViewModel>();
 
         public MainViewModel()
         {
@@ -31,16 +33,52 @@ namespace TrickleToTide.Mobile.ViewModels
             });
 
             MessagingCenter.Subscribe<PositionUpdate[]>(this, Constants.Message.POSITIONS_UPDATED, (positions)=> {
-                Log.Event($"{positions.Count()} positions");
+                MainThread.BeginInvokeOnMainThread(() => {
+                    foreach (var pos in positions)
+                    {
+                        var position = _positions.SingleOrDefault(p => p.Id == pos.Id);
+                        if (position == null)
+                        {
+                            position = new PositionViewModel()
+                            {
+                                Id = pos.Id,
+                                Address = "address",
+                                Description = pos.Nickname ?? "no-name",
+                                Position = new Xamarin.Forms.Maps.Position(pos.Latitude, pos.Longitude)
+                        };
+
+                            _positions.Add(position);
+                        }
+
+                        position.Address = "address";
+                        position.Description = pos.Nickname ?? "no-name";
+                        position.Position = new Xamarin.Forms.Maps.Position(pos.Latitude, pos.Longitude);
+                    }
+                });
             });
         }
 
-
+        public ObservableCollection<PositionViewModel> Positions => _positions;
 
         private Command _startCommand;
         public Command StartCommand => _startCommand ?? (_startCommand = new Command(_ => _updates.Start(), _ => !_updates.IsRunning && _updates.IsGpsConnected));
 
         private Command _stopCommand;
         public Command StopCommand => _stopCommand ?? (_stopCommand = new Command(_ => _updates.Stop(), _ => _updates.IsRunning && _updates.IsGpsConnected));
+    }
+
+
+    class PositionViewModel : BaseViewModel
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    
+        private Xamarin.Forms.Maps.Position _position;
+        public Xamarin.Forms.Maps.Position Position
+        {
+            get { return _position; }
+            set { SetProperty(ref _position, value); }
+        }
     }
 }
--- Mobile/TrickleToTide.Mobile/Views/MainPage.xaml.cs
+++ Mobile/TrickleToTide.Mobile/Views/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrickleToTide.Common;
 using TrickleToTide.Mobile.Delegates;
 using TrickleToTide.Mobile.ViewModels;
 using Xamarin.Essentials;
@@ -26,9 +27,21 @@ namespace TrickleToTide.Mobile.Views
             var lon = Preferences.Get(Constants.Preferences.LAST_LONGITUDE, -0.235422);
             map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(lat, lon), new Distance(100)));
 
-            MessagingCenter.Subscribe<GpsDelegate, IGpsReading>(this, Constants.Message.LOCATION_UPDATED, (sender, reading) =>
+            MessagingCenter.Subscribe<PositionUpdate[]>(this, Constants.Message.POSITIONS_UPDATED, (positions) =>
             {
-                map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(reading.Position.Latitude, reading.Position.Longitude), new Distance(100)));
+                if (positions.Any())
+                {
+                    var maxLat = positions.Max(p => p.Latitude);
+                    var minLat = positions.Min(p => p.Latitude);
+                    var maxLon = positions.Max(p => p.Longitude);
+                    var minLon = positions.Min(p => p.Longitude);
+
+                    var centreLat = (minLat + maxLat) / 2;
+                    var centreLon = (minLon + maxLon) / 2;
+                    var distance = GeoCode.CalcDistance(minLat, minLon, maxLat, maxLon, GeoCodeCalcMeasurement.Kilometers);
+
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(centreLat, centreLon), Distance.FromKilometers(distance)));
+                }
             });
         }
     }
